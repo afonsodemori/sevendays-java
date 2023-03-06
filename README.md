846c7b1f3107fdede45451bc30463fabdbf4aa04
Afonso de Mori <me@afonso.dev>
2023-03-06 22:01:14 +0100

    feat(day-1): Consuming an endpoint and writing its response in the terminal

---
+++ README.md
@@ -0,0 +1,18 @@
+# 7 Days Of Code: Java
+
+**7 Days Of Code** is a webpage maintained by [Alura](https://www.alura.com.br), a Brazilian Technology School, with a
+set of code challenges divided by technology (language, framework, tool, etc.) so the student can put in practice
+their knowledge solving 7 challenges, one by day, in the technology they're learning.
+
+**Learn more:** https://7daysofcode.io/matricula/java
+
+## Day 1
+
+On the first day, you will make your Java code consume the IMDB API, fetch the top 250 movies from their list and print
+the corresponding JSON in the console.
+
+### References
+
+* [IMDB API](https://imdb-api.com/api)
+* [OpenJDK.org: Introduction to the Java HTTP Client](https://openjdk.org/groups/net/httpclient/intro.html)
+* [Java System.getProperty vs System.getenv](https://www.baeldung.com/java-system-get-property-vs-system-getenv)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpTimeoutException;
+import java.time.Duration;
+
 public class Main {
+
+    private static final int REQUEST_TIMEOUT = 10;
+    private static final String API_KEY = System.getenv("IMDB_API_KEY");
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String endpoint = "https://imdb-api.com/en/API/Top250Movies/" + API_KEY;
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endpoint))
+                .timeout(Duration.ofSeconds(REQUEST_TIMEOUT))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, BodyHandlers.ofString()); // @see sendAsync
+            System.out.println(response.body());
+        } catch (HttpTimeoutException e) {
+            System.out.println("IMDB API is taking too long to respond. Try again later.");
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
