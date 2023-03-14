b54e201e17b15fcd9d3d465da94fe390a4650e8d
Afonso de Mori <me@afonso.dev>
2023-03-14 23:11:42 +0100

    feat(day-6): Implement MarvelComicJsonParser

--- src/MarvelComicJsonParser.java
+++ src/MarvelComicJsonParser.java
@@ -1,14 +1,75 @@
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MarvelComicJsonParser implements JsonParser {
     public List<Content> parse(String json) {
-        // TODO: This response is much more complex... work on it...
         ArrayList<Content> contents = new ArrayList<>();
 
-        contents.add(new Comic("Fake Info", 0, URI.create("https://i.annihil.us/u/prod/marvel/i/mg/c/80/5e3d7536c8ada.jpg"), 0.0));
+        Matcher matcher = Pattern.compile("\"results\":\\[(.*)]}}$").matcher(json);
+        if (!matcher.find()) {
+            // TODO: Give it some attention
+            return contents;
+        }
+
+        String group = matcher.group(1); // TODO: Learn more about Matcher
+
+        // Each Comic will miss {"id" and will have a comma (,) or curly bracket (}) in the end of the string
+        String[] comics = group.split("\\{?\"id\"");
+
+        for (String comic : comics) {
+            if (comic.length() == 0) {
+                // First one is empty
+                continue;
+            }
+
+            String fixedComic = "{\"id\"" + comic.substring(0, comic.length() - 1);
+
+            String title = getTitle(fixedComic);
+            URI image = URI.create(getImage(fixedComic));
+
+            // TODO: Should not be obligated to have year and rating attributes...
+            contents.add(new Comic(title, 0, image, 0.0));
+        }
 
         return contents;
+    }
+
+    private String getTitle(String comic) {
+        Matcher matcher = Pattern
+                .compile("\"title\":\"(.*)\",\"issueNumber\"")
+                .matcher(comic);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return null;
+    }
+
+    private String getImage(String comic) {
+        String path = "";
+        Matcher pathMatcher = Pattern
+                .compile("\"thumbnail\":\\{\"path\":\"([^\"]*)\",")
+                .matcher(comic);
+
+        if (pathMatcher.find()) {
+            path = pathMatcher.group(1);
+        }
+
+        String extension = "";
+        Matcher extensionMatcher = Pattern
+                .compile("\"thumbnail\":\\{.*,\"extension\":\"([^\"]*)\"")
+                .matcher(comic);
+
+        if (extensionMatcher.find()) {
+            extension = extensionMatcher.group(1);
+        }
+
+        return "%s.%s".formatted(path, extension);
     }
 }
